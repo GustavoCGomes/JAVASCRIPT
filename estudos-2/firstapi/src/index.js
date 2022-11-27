8d67a10233121ede331cbf0a65a77cda5aa632d0
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-11-27 19:28:17 -0300

    Users implementados

--- estudos-2/firstapi/src/index.js
+++ estudos-2/firstapi/src/index.js
@@ -1,8 +1,22 @@
+//criando a HTTP
 const http = require('http');
 
+//importando listagem de usuários
+const users = require('./mocks/users')
+
+//local host de chamada server
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Cintent-Type': 'text/html'});
-  response.end('<h1>Hello world!</h1>');
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if (request === '/users' && request.method === 'GET') {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+  //response.writeHead(200, {'Content-Type': 'text/html'});
+  //response.end('<h1>Hello world!</h1>');
 });
 
 server.listen(3000, () => console.log ('😎Server started at http://localhost:3000'));
---
+++ estudos-2/firstapi/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Gustavo'
+  },
+  {
+    id: 2,
+    name: 'Gomes'
+  },
+  {
+    id: 3,
+    name: 'Costa'
+  },
+]
