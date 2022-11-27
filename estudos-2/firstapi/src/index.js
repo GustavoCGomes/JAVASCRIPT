d0807bf68e096988c06eb6cb21659135a3f196f3
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-11-27 17:32:33 -0300

    Refazendo o código novamente

--- estudos-2/firstapi/src/index.js
+++ estudos-2/firstapi/src/index.js
@@ -1,22 +1,8 @@
-const  http = require('http');
+const http = requeri('http');
 
-const users = require(`./mocks/users`);
-
-// Criando o servidor
-const server = createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
-
-  if(request.url === '/users' && request.method === 'GET'){
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(users));
-  } else {
-      response.writeHead(404, { 'Content-Type': 'text/html' });
-      response.end(`Cannot ${request.method} ${request.url}`);
-    }
-  //response.writeHead(200, { 'Content-Type': 'text/html' });
-  //response.end('<h1>Hello world!</h1>')
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Cintent-Type': 'text/html'});
+  response.end('<h1>Hello world!</h1>');
 });
 
-server.listen(3000, () => {
-  return console.log('😎 Server started at http://localhost:3000');
-});
+server.listen(3000, () => console.log ('😎Server started at http://localhost300'));
