e9c87ab23385766c25949af249e1d2b2a6b970c6
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-11-27 18:47:52 -0300

    Debug corrigido na aplicação

--- estudos-2/firstapi/src/index.js
+++ estudos-2/firstapi/src/index.js
@@ -1,8 +1,8 @@
-const http = requeri('http');
+const http = require('http');
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Cintent-Type': 'text/html'});
   response.end('<h1>Hello world!</h1>');
 });
 
-server.listen(3000, () => console.log ('😎Server started at http://localhost300'));
+server.listen(3000, () => console.log ('😎Server started at http://localhost:3000'));
