3f03800b146d76c8afaaffe7291c6ebc21fd0793
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-12-14 17:47:47 -0300

    Organizando a API

---
+++ estudos-2/firstapi/src/controllers/UserController.js
@@ -0,0 +1,9 @@
+//importando listagem de usuários
+const users = require('../mocks/users');
+
+module.exports = {
+  listUsers(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+  }
+}
--- estudos-2/firstapi/src/index.js
+++ estudos-2/firstapi/src/index.js
@@ -1,20 +1,29 @@
 //criando a HTTP
 const http = require('http');
+const routes = require('./routes');
+const routes = require('./routes');
 
-//importando listagem de usuários
-const users = require('./mocks/users')
+const routes = require('./routes');
 
 //local host de chamada server
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if (request === '/users' && request.method === 'GET') {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(users));
+  const routes = routes.find((routesObj)=> (
+    routesObj.enpoint === request.url && routesObj.method === request.method
+  ));
+
+  if (route) {
+    route.handler(request, response);
   } else {
     response.writeHead(404, {'Content-Type': 'text/html'});
     response.end(`Cannot ${request.method} ${request.url}`);
-  }
+   }
+  
+  // if (request === '/users' && request.method === 'GET') {
+  //   UserController.listUsers(request, response);
+   
+  
   //response.writeHead(200, {'Content-Type': 'text/html'});
   //response.end('<h1>Hello world!</h1>');
 });
